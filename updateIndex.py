761db0d898a560e41ea236f365e479e7a9e6b623
ack8006 <takata.alex@gmail.com>
2015-09-05 14:50:22 -0400

    fixed some weird bugs

--- updateIndex.py
+++ updateIndex.py
@@ -6,37 +6,38 @@ from update import CheckUpdate
 import dbconnection as db
 
 
-def loopAssetDates(assetsDates):
+def loopAssetDates(indexDBs, assetsDates):
     for ad in assetsDates:
         code = ad[0]
         date = datetime.date(1900,1,1)
         if ad[1]: date = ad[1]+datetime.timedelta(days=1)
-
+        print ad
         priceData = getPriceData(code, date)
         if isinstance(priceData, pd.DataFrame):
-            storePriceData(code, priceData)
+            storePriceData(indexDBs, priceData)
 
 def getPriceData(code, date):
     priceData = None
     try:
         priceData = web.DataReader("^"+code,'yahoo',date)
-        priceData = cleanPriceData(priceData)
+        priceData = cleanPriceData(code, priceData)
     except IOError as e:
         print e
         print 'New Data Unavailable ' + code
     return priceData
 
 
-def storePriceData(code, priceData):
+def storePriceData(indexDBs, priceData):
     engine = db.startEngine()
     priceData.to_sql(indexDBs[1], engine, if_exists='append')
 
 
-def cleanPriceData(priceData):
+def cleanPriceData(code, priceData):
     priceData = priceData.drop('Adj Close',1)
     priceData.columns = map(str.lower, priceData.columns)
     priceData.index.rename('date', inplace=True)
     priceData['code']=code
+    return priceData
 
 
 def updateIndices():
@@ -44,8 +45,9 @@ def updateIndices():
     cu = CheckUpdate(indexDBs[0],indexDBs[1])
     newAssets = cu.getNewAssetForUpdate()
     existingAssets = cu.getExistingAssetsForUpdate()
+    print existingAssets
 
     assetsDates = newAssets+existingAssets
-    loopAssetDates(assetsDates)
+    loopAssetDates(indexDBs, assetsDates)
 
 
